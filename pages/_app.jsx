37e549dbb0d93b9b94afb74df21927d7eaab0b3d
twentyOne2x <contact@attn.market>
2025-11-15 14:32:10 +0100

    fix: build

---
+++ pages/_app.jsx
@@ -0,0 +1,6 @@
+import 'nextra-theme-docs/style.css'
+
+// Custom App required by Nextra v3 to include the theme styles
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
